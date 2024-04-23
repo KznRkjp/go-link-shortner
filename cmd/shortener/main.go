c3fed670c683a780ad9370676b6d918d1a329ca1
Mikhail <2424324@gmail.com>
2024-04-23 12:18:30 +0300

    logging_v1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,34 +4,24 @@ import (
 	"net/http"
 
 	"github.com/KznRkjp/go-link-shortner.git/internal/flags"
+	"github.com/KznRkjp/go-link-shortner.git/internal/middleware/shortlogger"
 	"github.com/KznRkjp/go-link-shortner.git/internal/router"
-	"github.com/KznRkjp/go-link-shortner.git/internal/shortlogger"
-	// "go.uber.org/zap"
 )
 
-// var sugar zap.SugaredLogger
-
 func main() {
-	// создаём предустановленный регистратор zap
-	// logger, err := zap.NewDevelopment()
-	// if err != nil {
-	// 	// вызываем панику, если ошибка
-	// 	panic(err)
-	// }
-	// sugar = *logger.Sugar()
-	// defer logger.Sync()
-	shortlogger.Test1()
+
 	flags.ParseFlags()
 	dd := router.Main()
+	shortlogger.ServerStartLog(flags.FlagRunAddr)
 
 	// fmt.Println("Server is listening @", flags.FlagRunAddr)
 	// fmt.Println("Press Ctrl+C to stop")
 	// log.Fatal(http.ListenAndServe(flags.FlagRunAddr, dd))
 	// записываем в лог, что сервер запускается
-	shortlogger.Sugar.Infow(
-		"Starting server",
-		"addr", flags.FlagRunAddr,
-	)
+	// shortlogger.Sugar.Infow(
+	// 	"Starting server",
+	// 	"addr", flags.FlagRunAddr,
+	// )
 	if err := http.ListenAndServe(flags.FlagRunAddr, dd); err != nil {
 		// записываем в лог ошибку, если сервер не запустился
 		shortlogger.Sugar.Fatalw(err.Error(), "event", "start server")
--- internal/router/router.go
+++ internal/router/router.go
@@ -2,12 +2,15 @@ package router
 
 import (
 	"github.com/KznRkjp/go-link-shortner.git/internal/app"
+	"github.com/KznRkjp/go-link-shortner.git/internal/middleware/shortlogger"
 	"github.com/go-chi/chi/v5"
+	// "github.com/go-chi/chi/v5/middleware"
 	// "github.com/KznRkjp/go-link-shortner.git/internal/flags"
 )
 
 func Main() chi.Router {
 	r := chi.NewRouter()
+	r.Use(shortlogger.WithLogging)
 	r.Post("/", app.GetURL)
 	r.Get("/{id}", app.ReturnURL)
 	return r
