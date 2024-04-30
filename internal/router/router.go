218b78120a5fdec42908511aeb9f69cb81393d62
Mikhail <2424324@gmail.com>
2024-04-30 17:54:58 +0300

    bugfix

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/KznRkjp/go-link-shortner.git/internal/app"
+	"github.com/KznRkjp/go-link-shortner.git/internal/middleware/gzipper"
 	"github.com/KznRkjp/go-link-shortner.git/internal/middleware/middlelogger"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
@@ -11,8 +12,8 @@ func Main() chi.Router {
 	r := chi.NewRouter()
 	r.Use(middlelogger.WithLogging)
 	r.Use(middleware.Compress(5))
-	r.Post("/", app.GetURL)
-	r.Get("/{id}", app.ReturnURL)
-	r.Post("/api/shorten", app.APIGetURL)
+	r.Post("/", gzipper.GzipMiddleware(app.GetURL))
+	r.Get("/{id}", gzipper.GzipMiddleware(app.ReturnURL))
+	r.Post("/api/shorten", gzipper.GzipMiddleware(app.APIGetURL))
 	return r
 }
