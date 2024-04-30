a504c930dcd4a5d84849ec3e81fce7a47a883aef
Mikhail <2424324@gmail.com>
2024-04-30 15:01:15 +0300

    +gzip

--- internal/router/router.go
+++ internal/router/router.go
@@ -3,12 +3,14 @@ package router
 import (
 	"github.com/KznRkjp/go-link-shortner.git/internal/app"
 	"github.com/KznRkjp/go-link-shortner.git/internal/middleware/middlelogger"
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 )
 
 func Main() chi.Router {
 	r := chi.NewRouter()
 	r.Use(middlelogger.WithLogging)
+	r.Use(middleware.Compress(5))
 	r.Post("/", app.GetURL)
 	r.Get("/{id}", app.ReturnURL)
 	r.Post("/api/shorten", app.APIGetURL)
