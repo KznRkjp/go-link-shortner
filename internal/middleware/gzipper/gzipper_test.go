7cf268cd36db806e969e3231a5e9e7158a9f75a3
Mikhail <2424324@gmail.com>
2024-07-23 15:44:10 +0300

    12

--- internal/middleware/gzipper/gzipper_test.go
+++ internal/middleware/gzipper/gzipper_test.go
@@ -2,7 +2,6 @@ package gzipper
 
 import (
 	"net/http"
-	"reflect"
 	"testing"
 )
 
@@ -19,7 +18,7 @@ func Test_newCompressWriter(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := newCompressWriter(tt.args.w); !reflect.DeepEqual(got, tt.want) {
+			if got := newCompressWriter(tt.args.w); got != tt.want {
 				t.Errorf("newCompressWriter() = %v, want %v", got, tt.want)
 			}
 		})
