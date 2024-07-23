b639b049ad873f4bd15769804efd6affac02bee2
Mikhail <2424324@gmail.com>
2024-07-23 14:45:17 +0300

    123

--- internal/app/app_test.go
+++ internal/app/app_test.go
@@ -14,14 +14,22 @@ func Test_saveData(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-		want string
+		want int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Test #1",
+			args: args{
+				ctx:  context.Background(),
+				body: []byte("http://mail.ru"),
+				uuid: "1dsf24123123123",
+			},
+			want: 1,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := saveData(tt.args.ctx, tt.args.body, tt.args.uuid); got != tt.want {
-				t.Errorf("saveData() = %v, want %v", got, tt.want)
+			if got := saveData(tt.args.ctx, tt.args.body, tt.args.uuid); len([]rune(got)) < tt.want {
+				t.Errorf("saveData() = %v, length is less than %v", got, tt.want)
 			}
 		})
 	}
